05bbccd9f96b8c7b2bc0bcd95e71b5c30a3475ed
Bilel Gattoufi <gtf-bll@hotmail.com>
2018-10-10 12:52:09 +0100

    header et footer

---
+++ footer.php
@@ -0,0 +1,28 @@
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Pied de page </title>
+</head>
+
+<body>
+
+<footer>
+
+<TABLE BORDER="0">
+  <TR>
+    <TD align="left" WIDTH="600" ><h4 style="color:Tomato;">Adresse : Tunisie Tunis 1002  </h4></TD>
+    <TD width="300"><a href ="../index.html"> Acceuil</TD>
+    <td width="350"><a href="../controller/ajouter.php">Ajouter Nouvelle service</a></td>
+    <td width="350"><a href="../view/liste.php">voir liste service</a></td>
+    <TD align="right" WIDTH="800"> <font face="Times New Roman"> <h4 style="color:MediumSeaGreen;" >Contact : 71 33 22 11 55  </h4> </font></TD>
+  </TR>
+</TABLE>
+
+</footer>
+	
+</body>
+</html>
+
---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title></title>
+</head>
+
+    <body style="background-color:#E6E2AF">
+
+<header>
+	<h1 style="color:SlateBlue;"> <center> Bienvenue merci de visiter notre site </center></h1>
+	<br /> 
+	<hr style="color:tomato;" />
+</header>
+
+</body1>
+</html>
